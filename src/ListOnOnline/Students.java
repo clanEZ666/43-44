4db5588624bd491f01fadffddf23a096e7cde540
ClanEZ <bernardosoto30rus@gmail.com>
2024-11-21 18:31:51 +0400

    43-44 (1)

---
+++ src/ListOnOnline/Students.java
@@ -0,0 +1,18 @@
+package ListOnOnline;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Students {
+
+    private final List<String> students = new ArrayList<>();
+
+    public void addStudent(List String) {}
+
+
+
+
+
+
+
+}
